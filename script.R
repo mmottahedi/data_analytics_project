78e6d3a23c72ec76921c4e3f0a0627972c4b7b62
mmottahedi <m.mottahedi@gmail.com>
2015-04-18 21:58:06 -0500

    tree based model added

--- script.R
+++ script.R
@@ -3,7 +3,19 @@ setwd("/home/mfc/cwd/Data_project/Data_project")
 
 #load packages
 require(ggplot2)
+library(tree)
+
 
 #load data
 train <- read.csv("train.csv",header = T)
+
+hist(train$feat_1)
+
+#tree based method
+
+tree.otto = tree(target ~ . -id, data=train)
+plot(tree.otto)
+text(tree.otto,pretty=0)
+summary(tree.otto)
+
 
