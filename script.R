aff858320884f4cf82b4bba1fa18b4170b568069
mmottahedi <m.mottahedi@gmail.com>
2015-04-21 22:44:32 -0500

    cv added

--- script.R
+++ script.R
@@ -1,5 +1,6 @@
 # set cwd
 setwd("/home/mfc/cwd/Data_project/Data_project")
+set.seed(1234)
 
 #load packages
 require(ggplot2)
@@ -7,8 +8,8 @@ library(tree)
 
 
 #load data
-train <- read.csv("train.csv",header = T)
-
+train <- read.csv("train.csv",header = T )#, stringsAsFactors=FALSE)
+test <- read.csv("test.csv")
 hist(train$feat_1)
 
 #tree based method
@@ -16,6 +17,18 @@ hist(train$feat_1)
 tree.otto = tree(target ~ . -id, data=train)
 plot(tree.otto)
 text(tree.otto,pretty=0)
-summary(tree.otto)
+print(summary(tree.otto))
+tree.pred <- predict( tree.otto,test,type="class")
+cv.trees = cv.tree(tree.otto,FUN=prune.misclass)
+pruned.tree <- prune.misclass(tree.otto,best=0)
+plot(pruned.tree)
+text(pruned.tree,pretty=0)
+
+
+par( mfrow = c (1 ,2) )
+plot( cv.trees$size , cv.tree$dev , type ="b")
+plot( cv.trees$k , cv.tree$dev , type ="b")
+
+
 
 
