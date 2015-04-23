767d0f336c05c89a046505b713329a9a6396b8fa
mmottahedi <m.mottahedi@gmail.com>
2015-04-23 10:45:37 -0500

    random forest added

--- script.R
+++ script.R
@@ -5,12 +5,18 @@ set.seed(1234)
 #load packages
 require(ggplot2)
 library(tree)
-
+library(readr)
+library(randomForest)
 
 #load data
 train <- read.csv("train.csv",header = T )#, stringsAsFactors=FALSE)
 test <- read.csv("test.csv")
-hist(train$feat_1)
+
+submission.tree <- data.frame(id=test$id, Class_1=NA, Class_2=NA, Class_3=NA, Class_4=NA, Class_5=NA, Class_6=NA, Class_7=NA, Class_8=NA, Class_9=NA)
+
+submission.rf <- data.frame(id=test$id, Class_1=NA, Class_2=NA, Class_3=NA, Class_4=NA, Class_5=NA, Class_6=NA, Class_7=NA, Class_8=NA, Class_9=NA)
+
+#hist(train$feat_1)
 
 #tree based method
 
@@ -25,10 +31,28 @@ plot(pruned.tree)
 text(pruned.tree,pretty=0)
 
 
-par( mfrow = c (1 ,2) )
-plot( cv.trees$size , cv.tree$dev , type ="b")
-plot( cv.trees$k , cv.tree$dev , type ="b")
+par(mfrow = c(1 ,2))
+plot(cv.trees$size , cv.trees$dev , type ="b")
+plot(cv.trees$k , cv.trees$dev , type ="b")
 
 
 
+#random FOrest
+
+rf <- randomForest(train[,c(-1,-95)], as.factor(train$target), ntree=25,importance=TRUE)
+submission.rf[,2:10] <- (predict(rf, test[,-1], type="prob"))
+write.csv(submission.rf,"rf.submission.csv")
+imp <- importance(rf, type=1)
+featureImportance <- data.frame(Feature=row.names(imp), Importance=imp[,1])
+
+p <- ggplot(featureImportance, aes(x=reorder(Feature, Importance), y=Importance)) +
+        geom_bar(stat="identity", fill="#53cfff") +
+        coord_flip() +
+        theme_light(base_size=20) +
+        xlab("Importance") +
+        ylab("") +
+        ggtitle("Random Forest Feature Importance\n") +
+        theme(plot.title=element_text(size=18))
+
+ggsave("2_feature_importance.png", p, height=20, width=8, units="in")
 
